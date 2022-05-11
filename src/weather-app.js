bc313d5a1e0f9f915c0ca4b380af5aa0fb280362
bjpiasecka <104096311+bjpiasecka@users.noreply.github.com>
2022-05-11 20:40:52 +0200

    Added API to my js file and connected it with html file

---
+++ src/weather-app.js
@@ -0,0 +1,22 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let description = document.querySelector("#description");
+  description.innerHTML = response.data.weather[0].description;
+  let humidity = document.querySelector("#humidity");
+  humidity.innerHTML = response.data.main.humidity;
+  let wind = document.querySelector("#wind");
+  wind.innerHTML = Math.round(response.data.wind.speed);
+  let pressure = document.querySelector("#pressure");
+  pressure.innerHTML = response.data.main.pressure;
+  console.log(response.data);
+}
+
+let apiKey = "c8a6e0907a9f2de302f3030eaf713d4b";
+
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
