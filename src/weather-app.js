742f15da5b77f05ed5cda04ceea9cd098b40fa32
bjpiasecka <104096311+bjpiasecka@users.noreply.github.com>
2022-05-11 21:43:03 +0200

    Styling h3

--- src/weather-app.js
+++ src/weather-app.js
@@ -1,25 +1,29 @@
-function formatDate(timestamp) {
-  let date = new Date(timestamp);
-  let hours = date.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-  let minutes = date.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  let day = days[date.getDay()];
-  return `${day} ${hours}:${minutes}`;
+//Day & hour
+let now = new Date();
+let h2 = document.querySelector("h2");
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+
+let hour = now.getHours();
+if (hour < 10) {
+  hour = `0${hour}`;
 }
+
+let minute = now.getMinutes();
+if (minute < 10) {
+  minute = `0${minute}`;
+}
+
+h2.innerHTML = `${day} ${hour}:${minute}`;
 
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
@@ -34,8 +38,6 @@ function displayTemperature(response) {
   wind.innerHTML = Math.round(response.data.wind.speed);
   let pressure = document.querySelector("#pressure");
   pressure.innerHTML = response.data.main.pressure;
-  let date = document.querySelector("#date");
-  date.innerHTML = formatDate(response.data.dt * 1000);
   console.log(response.data);
 }
 
