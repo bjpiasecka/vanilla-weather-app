b973c636ab0723abc030a9e37341e43fb56f4c50
bjpiasecka <104096311+bjpiasecka@users.noreply.github.com>
2022-05-11 21:16:14 +0200

    Creating the time

--- src/weather-app.js
+++ src/weather-app.js
@@ -1,3 +1,26 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
@@ -11,12 +34,14 @@ function displayTemperature(response) {
   wind.innerHTML = Math.round(response.data.wind.speed);
   let pressure = document.querySelector("#pressure");
   pressure.innerHTML = response.data.main.pressure;
+  let date = document.querySelector("#date");
+  date.innerHTML = formatDate(response.data.dt * 1000);
   console.log(response.data);
 }
 
 let apiKey = "c8a6e0907a9f2de302f3030eaf713d4b";
-
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let city = "London";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
